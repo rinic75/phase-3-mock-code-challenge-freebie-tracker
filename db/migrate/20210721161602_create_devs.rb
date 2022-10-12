1903abcab4a2df27b7c696517ec53d978190a1f5
Chang Shin <cshin7374@gmail.com>
2022-10-11 21:32:37 -0500

    commit tree 108d85770df92a067555cbf330b5901a6675c5a8

--- db/migrate/20210721161602_create_devs.rb
+++ db/migrate/20210721161602_create_devs.rb
@@ -3,5 +3,14 @@ class CreateDevs < ActiveRecord::Migration[6.1]
     create_table :devs do |t|
       t.string :name
     end
+    def received_one?(item_name)
+      self.freebies.any? do |f|
+        f.item_name == item_name
+      end
+    end
+    def give_away(freebie, dev)
+      freebie.update(dev: dev) unless freebie.dev != self
+    end
+  
   end
 end
---
+++ db/migrate/20221012022412_add_company_dev_id_column.rb
@@ -0,0 +1,6 @@
+class AddCompanyDevIdColumn < ActiveRecord::Migration[6.1]
+  def change
+    add_column :freebies, :company_id, :integer
+    add_column :freebies, :dev_id, :integer
+  end
+end
